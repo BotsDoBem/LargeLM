9cf28088847662b2ceb29a1861aab95b52d606b5
Thiago <thiago.ferreira@aixplain.com>
2022-03-06 13:09:49 -0800

    Bots do Bem in English

--- botsdobem/load_data_en.py
+++ botsdobem/load_data_en.py
@@ -7,31 +7,23 @@ from sklearn.model_selection import train_test_split
 from torch.utils.data import DataLoader, Dataset
 from num2words import num2words
 import stanza
-stanza.download('pt') 
+stanza.download('en') 
 
-DOMAINS = ['covid19', 'deforestation_daily', 'deforestation_month', 'amazon_fire']#, 'ibge_ipca']
+DOMAINS = ['deforestation_daily', 'deforestation_month']
 
 DOMAIN2TOKEN = {
-    'covid19': '[COVID19]',
     'deforestation_daily': '[DEFORESTATION_DAILY]',
-    'deforestation_month': '[DEFORESTATION_MONTH]',
-    'amazon_fire': '[AMAZON_FIRE]',
-    # 'ibge_ipca': '[IBGE_IPCA]',
+    'deforestation_month': '[DEFORESTATION_MONTH]'
 }
 
 DOMAIN2PATH = {
-    'covid19': 'covid19.json',
     'deforestation_daily': 'deter_daily.json',
-    'deforestation_month': 'deter_month.json',
-    'amazon_fire': 'amazon_fire.json',
-    # 'ibge_ipca': 'ibge_ipca.json',
+    'deforestation_month': 'deter_month.json'
 }
 
 DOMAIN2PATH_SYNTHETIC = {
-    'covid19': 'covid19',
     'deforestation_daily': 'amazondaily',
     'deforestation_month': 'amazonmonth',
-    'amazon_fire': 'firedata',
 }
 
 TEST_SPLIT=0.2
@@ -45,11 +37,11 @@ def format_intent(message, domain, describe_numbers=False):
         if str(value.replace('.', '')).isnumeric():
             value = float(value)
             # fix inconsistencies with percentages in covid and deforestation
-            if attr == 'variation' and domain in ['covid19', 'deforestation_daily', 'deforestation_month']:
+            if attr == 'variation' and domain in ['deforestation_daily', 'deforestation_month']:
                 value = float(value) * 100
             value = int(value) if value.is_integer() else round(value, 2)
             if describe_numbers:
-                value = num2words(value, lang='pt-br')
+                value = num2words(value, lang='en')
             else:
                 value = str(value)
       
@@ -88,34 +80,13 @@ def format_text(sentence, describe_numbers=False, nlp=None):
     for number in numbers:
         if describe_numbers:
             try:
-                desc = num2words(number.replace('.', '').replace(',', '.'), lang='pt-br')
+                desc = num2words(number, lang='en')
                 sentence = sentence.replace(number, desc, 1)
             except:
                 pass
     if describe_numbers:
-        sentence = sentence.replace('%', ' por cento')
+        sentence = sentence.replace('%', ' percent')
     return sentence
-#     tokens = sentence.split()
-#     for i, token in enumerate(tokens):
-#         if str(token.replace('.', '')).isnumeric():
-#             punctuation = False
-#             if token[-1] == '.':
-#                 token = token[:-1]
-#             try:
-#                 value = float(token)
-#                 value = int(value) if value.is_integer() else round(value, 2)
-#                 if describe_numbers:
-#                     value = num2words(value, lang='pt-br')
-#                 else:
-#                     value = str(value)
-#                 value = str(value)
-#             except:
-#                 value = token
-#             if punctuation:
-#                 value += '.'
-            
-#             tokens[i] = value
-#     return ' '.join(tokens)
 
 def preprocess(data, domain, setting, describe_number_src=False, describe_number_trg=False, nlp=None):
     """
@@ -145,49 +116,31 @@ def preprocess(data, domain, setting, describe_number_src=False, describe_number
                 result.append({ 'X': DOMAIN2TOKEN[domain] + ' ' + inp, 'y': snt_text })
     return result
 
-def load(setting='original', describe_number_src=False, describe_number_trg=False):
+def load(setting='synthetic', describe_number_src=False, describe_number_trg=False):
     """
     Args:
         setting: 
             original - only human-annotated data
             synthetic - original + synthetic data
     """
-    assert setting in ['original', 'synthetic']
+    assert setting in ['synthetic']
     nlp = None
     if describe_number_trg:
-        nlp = stanza.Pipeline('pt')
+        nlp = stanza.Pipeline('en')
     
-    if setting == 'original':
-        if not os.path.exists('botsdobem/data/pt-br/original/traindata.json'):
-            traindata, testdata = [], []
-            for domain in DOMAINS:
-                data = json.load(open(os.path.join('botsdobem/data/pt-br', setting, DOMAIN2PATH[domain])))
-                shuffle(data)
-                size = int(len(data) * TEST_SPLIT)
-                domain_traindata, domain_testdata = data[size:], data[:size]
-                traindata.extend(preprocess(domain_traindata, domain, setting, describe_number_src, describe_number_trg, nlp))
-                testdata.extend(preprocess(domain_testdata, domain, setting, describe_number_src, describe_number_trg, nlp))
-            
-            json.dump(traindata, open('botsdobem/data/pt-br/original/traindata.json', 'w'))
-            json.dump(testdata, open('botsdobem/data/pt-br/original/devdata.json', 'w'))
-        else:
-            traindata = json.load(open('botsdobem/data/pt-br/original/traindata.json'))
-            testdata = json.load(open('botsdobem/data/pt-br/original/devdata.json'))
-        return traindata, testdata
-    else:
-        traindata, devdata, testdata = [], [], []
-        for domain in DOMAINS:
-            if domain != 'ibge_ipca':
-                print(domain)
-                path = os.path.join('botsdobem/data/pt-br', setting, DOMAIN2PATH_SYNTHETIC[domain])
-                domain_traindata = json.load(open(os.path.join(path, 'traindata.json')))
-                domain_devdata = json.load(open(os.path.join(path, 'devdata.json')))
-                domain_testdata = json.load(open(os.path.join(path, 'testdata.json')))
+    traindata, devdata, testdata = [], [], []
+    for domain in DOMAINS:
+        if domain != 'ibge_ipca':
+            print(domain)
+            path = os.path.join('botsdobem/data/en', setting, DOMAIN2PATH_SYNTHETIC[domain])
+            domain_traindata = json.load(open(os.path.join(path, 'traindata.json')))
+            domain_devdata = json.load(open(os.path.join(path, 'devdata.json')))
+            domain_testdata = json.load(open(os.path.join(path, 'testdata.json')))
 
-                traindata.extend(preprocess(domain_traindata, domain, setting, describe_number_src, describe_number_trg, nlp))
-                devdata.extend(preprocess(domain_devdata, domain, setting, describe_number_src, describe_number_trg, nlp))
-                testdata.extend(preprocess(domain_testdata, domain, setting, describe_number_src, describe_number_trg, nlp))
-        return traindata, devdata, testdata
+            traindata.extend(preprocess(domain_traindata, domain, setting, describe_number_src, describe_number_trg, nlp))
+            devdata.extend(preprocess(domain_devdata, domain, setting, describe_number_src, describe_number_trg, nlp))
+            testdata.extend(preprocess(domain_testdata, domain, setting, describe_number_src, describe_number_trg, nlp))
+    return traindata, devdata, testdata
 
 class NewsDataset(Dataset):
     def __init__(self, data):
---
+++ main_en.sh
@@ -0,0 +1,182 @@
+mkdir logs
+# original botsdobem (no_desc_src - no_desc_trg)
+python3 main.py facebook/bart-large facebook/bart-large \
+                'botsdobem synthetic' \
+                30 \
+                1e-5 \
+                1 \
+                5 \
+                300 \
+                logs_en/botsdobem_bart_nodesc \
+                english \
+                --verbose \
+                --batch_status 16 \
+                --cuda
+                
+# original botsdobem (desc_src - no_desc_trg)
+python3 main.py facebook/bart-large facebook/bart-large \
+                'botsdobem synthetic' \
+                30 \
+                1e-5 \
+                1 \
+                5 \
+                300 \
+                logs_en/botsdobem_bart_descsrc \
+                english \
+                --verbose \
+                --batch_status 16 \
+                --cuda \
+                --describe_number_src
+
+# original botsdobem (no_desc_src - desc_trg)
+python3 main.py facebook/bart-large facebook/bart-large \
+                'botsdobem synthetic' \
+                30 \
+                1e-5 \
+                1 \
+                5 \
+                300 \
+                logs_en/botsdobem_bart_desctrg \
+                english \
+                --verbose \
+                --batch_status 16 \
+                --cuda \
+                --describe_number_trg 
+                
+# original botsdobem (desc_src - desc_trg)
+python3 main.py facebook/bart-large facebook/bart-large \
+                'botsdobem synthetic' \
+                30 \
+                1e-5 \
+                1 \
+                5 \
+                300 \
+                logs_en/botsdobem_bart_desc \
+                english \
+                --verbose \
+                --batch_status 16 \
+                --cuda \
+                --describe_number_src \
+                --describe_number_trg
+
+# MT5
+# original botsdobem (no_desc_src - no_desc_trg)
+python3 main.py t5-base t5-base \
+                'botsdobem synthetic' \
+                30 \
+                1e-4 \
+                1 \
+                5 \
+                300 \
+                logs_en/botsdobem_t5_nodesc \
+                english \
+                --verbose \
+                --batch_status 16 \
+                --cuda
+                
+# original botsdobem (desc_src - no_desc_trg)
+python3 main.py t5-base t5-base \
+                'botsdobem synthetic' \
+                30 \
+                1e-4 \
+                1 \
+                5 \
+                300 \
+                logs_en/botsdobem_t5_descsrc \
+                english \
+                --verbose \
+                --describe_number_src \
+                --batch_status 16 \
+                --cuda
+                
+# original botsdobem (no_desc_src - desc_trg)
+python3 main.py t5-base t5-base \
+                'botsdobem synthetic' \
+                30 \
+                1e-4 \
+                1 \
+                5 \
+                300 \
+                logs_en/botsdobem_t5_desctrg \
+                english \
+                --verbose \
+                --describe_number_trg \
+                --batch_status 16 \
+                --cuda
+
+# original botsdobem (desc_src - desc_trg)
+python3 main.py t5-base t5-base \
+                'botsdobem synthetic' \
+                30 \
+                1e-4 \
+                1 \
+                5 \
+                300 \
+                logs_en/botsdobem_t5_desc \
+                english \
+                --verbose \
+                --describe_number_src \
+                --describe_number_trg \
+                --batch_status 16 \
+                --cuda
+
+# GPT-2
+# original botsdobem (no_desc_src - no_desc_trg)
+python3 main.py gpt2 gpt2 \
+                'botsdobem synthetic' \
+                30 \
+                1e-5 \
+                2 \
+                5 \
+                300 \
+                logs_en/botsdobem_gpt2_nodesc \
+                english \
+                --verbose \
+                --batch_status 16 \
+                --cuda
+                
+# original botsdobem (desc_src - no_desc_trg)
+python3 main.py gpt2 gpt2 \
+                'botsdobem synthetic' \
+                30 \
+                1e-5 \
+                2 \
+                5 \
+                300 \
+                logs_en/botsdobem_gpt2_descsrc \
+                english \
+                --verbose \
+                --describe_number_src \
+                --batch_status 16 \
+                --cuda
+
+# original botsdobem (no_desc_src - desc_trg)
+python3 main.py gpt2 gpt2 \
+                'botsdobem synthetic' \
+                30 \
+                1e-5 \
+                2 \
+                5 \
+                300 \
+                logs_en/botsdobem_gpt2_desctrg \
+                english \
+                --verbose \
+                --describe_number_trg \
+                --batch_status 16 \
+                --cuda
+
+# original botsdobem (desc_src - desc_trg)
+python3 main.py gpt2 gpt2 \
+                'botsdobem synthetic' \
+                30 \
+                1e-5 \
+                2 \
+                5 \
+                300 \
+                logs_en/botsdobem_gpt2_desc \
+                english \
+                --verbose \
+                --describe_number_src \
+                --describe_number_trg \
+                --batch_status 16 \
+                --cuda
